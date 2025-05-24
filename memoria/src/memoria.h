152e0f779912ed75ae3dc52424c290b24856349e
manuEsteban22 <manuestebanseret@gmail.com>
2025-05-24 05:07:45 +0000

    creo estructura para guardar memoria.congif

--- memoria/src/memoria.h
+++ memoria/src/memoria.h
@@ -2,4 +2,21 @@
 #define MEMORIA_H_
 
 extern t_log* logger;
+
+typedef struct
+{
+    char* puerto_escucha;
+    int tam_memoria;
+    int tam_pagina;
+    int entradas_por_tabla;
+    int cantidad_niveles;
+    int retardo_memoria;
+    char* path_swapfile;
+    int retardo_swap;
+    char* log_level;
+    char* dump_path;
+    char* path_instrucciones;
+} t_configuracion;
+
+extern t_configuracion campos_config;
 #endif
