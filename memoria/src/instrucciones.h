2b4a1b4daa61bd3a29432cf697df4e44adca2728
manuEsteban22 <manuestebanseret@gmail.com>
2025-05-24 05:55:16 +0000

    definí bien una función

--- memoria/src/instrucciones.h
+++ memoria/src/instrucciones.h
@@ -2,12 +2,13 @@
 #define INSTRUCCIONES_H_
 
 
-void devolver_instrucciones(int pid);
+void cargar_instrucciones(int pid);
 char* obtener_instruccion(int pc);
 int cantidad_instrucciones();
 int espacio_libre();
 void liberar_memoria();
 int mandar_instrucciones();
+
 
 
 
