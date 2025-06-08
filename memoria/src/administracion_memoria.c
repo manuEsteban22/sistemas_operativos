2a18a390f28df31948459f8fd17d2cee0cafbe71
manuEsteban22 <manuestebanseret@gmail.com>
2025-06-08 21:45:48 +0000

    termino funcion inicializar memoria

--- memoria/src/administracion_memoria.c
+++ memoria/src/administracion_memoria.c
@@ -15,7 +15,14 @@ void inicializar_memoria()
         log_info(logger, "Error al inicializar memoria");
         exit(EXIT_FAILURE);
     }
+
+    memset(memoria_usuario, 0, campos_config.tam_memoria);
+
     cantidad_marcos = campos_config.tam_memoria / campos_config.tam_pagina;
 
-    log_info(logger,"%d",cantidad_marcos);
+    bitmap_marcos = malloc(sizeof(bool) * cantidad_marcos);             //(reserva) crea array de bools para marcar cada marco
+    memset(bitmap_marcos, false, sizeof(bool) * cantidad_marcos);      // los pone a todos en false
+
+
+    log_info(logger,"Memoria de usuario inicializada con %d marcos de %db cada uno",cantidad_marcos, campos_config.tam_pagina);
 }
