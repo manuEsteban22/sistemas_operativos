39aa3eae92a1ae78b4f8237fa8c7182c091c73d4
sildileo <sdileo@frba.utn.edu.ar>
2025-06-20 20:19:11 +0000

    agrego funciones de swap leer y escribir + asignar completa

--- memoria/src/swap.h
+++ memoria/src/swap.h
@@ -3,10 +3,12 @@
 #include <stdio.h>
 #include <commons/log.h>
 #include <commons/collections/list.h>
-#include "config_memoria.h"
+#include <config_memoria.h>
 
 void inicializar_swap();
 t_list* asignar_marcos_swap(int cantidad);
+void escribir_en_swap(void* buffer, int nro_marco);
+void leer_de_swap(void* buffer, int nro_marco);
 void liberar_marcos(t_list* marcos);
 
 #endif
