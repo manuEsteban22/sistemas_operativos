3adee20dc435c1066c80a30f0b10d661bc55dc2b
manuEsteban22 <manuestebanseret@gmail.com>
2025-07-12 05:50:40 -0300

    hicimos que lo que se guarde en el diccionario sea el strcut proceso en vez de tabla raiz

--- memoria/src/atender_kernel.c
+++ memoria/src/atender_kernel.c
@@ -4,61 +4,44 @@ extern t_log* logger;
 t_dictionary* tablas_por_pid;
 t_list* paginas_en_swap;
 
-<<<<<<< HEAD
 // busca pags libres, las asigna y arma tabla raiz
-=======
-
-// busca pags libres, las asigna y arma tabla raiz
->>>>>>> 4cc24c82fb4ffc20da474d804199a3fc0b47bf93
 
 void* inicializar_proceso(int tam_proceso, int pid){
-
-<<<<<<< HEAD
-
-
     int pags_necesarias = tam_proceso / campos_config.tam_pagina;
-    t_tabla_paginas* tabla_raiz = crear_tabla(0);
-    dictionary_put(tablas_por_pid, string_itoa(pid), tabla_raiz);
-=======
-//     int pags_necesarias = tam_proceso / campos_config.tam_pagina;
-//     t_tabla_paginas* tabla_raiz = crear_tabla(0);
-//     dictionary_put(tablas_por_pid, string_itoa(pid), tabla_raiz);
->>>>>>> 4cc24c82fb4ffc20da474d804199a3fc0b47bf93
+    t_proceso* proceso = malloc(sizeof(t_proceso));
+    proceso->pid = pid;
+    proceso->tabla_raiz = crear_tabla(0);
+    proceso->paginas_usadas = pags_necesarias;
+    proceso->accesos_memoria = 0;
+    proceso->page_faults = 0;
+    memset(&proceso->metricas, 0, sizeof(t_metricas));
+    dictionary_put(tablas_por_pid, string_itoa(pid), proceso);
 
     for(int pagina = 0; pagina < pags_necesarias; pagina++){
-
         int marco_libre = buscar_marco_libre();
         if (marco_libre == -1){
             return NULL; //falta q haga algo
         }
-
         bitarray_set_bit(bitmap_marcos, marco_libre);
-        t_entrada_tabla* entrada = buscar_entrada(tabla_raiz, pagina);
-
+        t_entrada_tabla* entrada = buscar_entrada(proceso->tabla_raiz, pagina);
         entrada->presencia = true;
         entrada->marco = marco_libre;
     }
-
-    return tabla_raiz;
+    return proceso->tabla_raiz;
 }
 
 void suspender_proceso(int pid){
-
-
     char* pid_str = string_itoa(pid);
-    t_tabla_paginas* tabla_raiz = dictionary_get(tablas_por_pid, pid_str);
+    t_proceso* proceso = dictionary_get(tablas_por_pid, pid_str);
     free(pid_str);
-
-    if (!tabla_raiz){
-        log_error(logger, "No se encontró la tabla de páginas para el proceso %d", pid);
+    if (!proceso){
+        log_error(logger, "No se encontró el proceso %d", pid);
         return;
     }
-
-    suspender_tabla(tabla_raiz, 0, pid, 0);
+    suspender_tabla(proceso->tabla_raiz, 0, pid, 0);
 }
 
 void des_suspender_proceso(int pid){
-
     t_list* paginas_proceso = list_create(); //filtramos las paginas de ese proceso
     for (int i = 0; i < list_size(paginas_en_swap); i++){
         t_pagina_swap* relacion = list_get(paginas_en_swap, i);
@@ -66,105 +49,89 @@ void des_suspender_proceso(int pid){
             list_add(paginas_proceso, relacion);
         }
     }
-
-    if (list_size(paginas_proceso) > cantidad_marcos_libres()){ //error si no hay suficientes marcos libres
+    if (list_size(paginas_proceso) > cantidad_marcos_libres()){
         log_error(logger, "No hay marcos libres suficientes para des-suspender el proceso %d", pid);
         list_destroy(paginas_proceso);
         return;
     }
-
     char* pid_str = string_itoa(pid);
-    t_tabla_paginas* tabla_raiz = dictionary_get(tablas_por_pid, pid_str); //actualizar tabla de pags
-
-    for (int i = 0; i < list_size(paginas_proceso); i++) { //restaurar las pags
+    t_proceso* proceso = dictionary_get(tablas_por_pid, pid_str);
+    t_tabla_paginas* tabla_raiz = proceso->tabla_raiz;
+    for (int i = 0; i < list_size(paginas_proceso); i++) {
         t_pagina_swap* relacion = list_get(paginas_proceso, i);
         int marco_ram = buscar_marco_libre();
         if (marco_ram == -1) {
             log_error(logger, "Error inesperado: no hay marco libre para restaurar página");
             break;
         }
-
         void* buffer = malloc(campos_config.tam_pagina);
         leer_de_swap(buffer, relacion->marco_swap);
         memcpy(memoria_usuario + marco_ram * campos_config.tam_pagina, buffer, campos_config.tam_pagina);
-
-
         t_entrada_tabla* entrada = buscar_entrada(tabla_raiz, relacion->nro_pagina);
         entrada->presencia = true;
         entrada->marco = marco_ram;
-
-        t_list* marcos_swap = list_create(); //liberar marcos del swap
+        t_list* marcos_swap = list_create();
         int* marco_swap_ptr = malloc(sizeof(int));
         *marco_swap_ptr = relacion->marco_swap;
         list_add(marcos_swap, marco_swap_ptr);
         liberar_marcos(marcos_swap);
-
         list_remove_element(paginas_en_swap, relacion);
         free(relacion);
         free(buffer);
     }
     free(pid_str);
-
     list_destroy(paginas_proceso);
     log_info(logger, "Proceso %d des-suspendido correctamente", pid);
 }
 
 void* finalizar_proceso(int pid){
-
-<<<<<<< HEAD
-    char* pid_str = string_itoa(pid); //liberamos marcos de ram y marcamos las entradas como no presentes
-    t_tabla_paginas* tabla_raiz = dictionary_get(tablas_por_pid, pid_str);
-
-    if (!tabla_raiz){
-        log_error(logger, "No se encontró la tabla de páginas para el proceso %d", pid);
+    char* pid_str = string_itoa(pid);
+    t_proceso* proceso = dictionary_get(tablas_por_pid, pid_str);
+    if (!proceso){
+        log_error(logger, "No se encontró el proceso %d", pid);
         free(pid_str);
-        return;
+        return NULL;
     }
-
-    liberar_tabla(tabla_raiz, 0); 
-
-    for (int i = 0; i < list_size(paginas_en_swap); ){ //liberamos marcos de swap y elminamos las relaciones
+    liberar_tabla(proceso->tabla_raiz, 0);
+    for (int i = 0; i < list_size(paginas_en_swap); ){
         t_pagina_swap* relacion = list_get(paginas_en_swap, i);
         if (relacion->pid == pid){
             t_list* marcos_swap = list_create();
             int* marco_swap_ptr = malloc(sizeof(int));
             *marco_swap_ptr = relacion->marco_swap;
             list_add(marcos_swap, marco_swap_ptr);
-            liberar_marcos(marcos_swap;)
-
+            liberar_marcos(marcos_swap);
             list_remove(paginas_en_swap, i);
             free(relacion);
         } else {
-            i++; // se incrementa solo si no se elimino
+            i++;
         }
     }
-
-    dictionary_remove(tablas_por_pid, pid_str); //elimina tabla del diccionario
+    dictionary_remove(tablas_por_pid, pid_str);
     free(pid_str);
-
-    log_info(logger, "Proceso %d finalizado. ")
+    // Log de métricas
+    log_info(logger, "Proceso %d finalizado. Accesos: %d, Page Faults: %d, Pags usadas: %d", pid, proceso->accesos_memoria, proceso->page_faults, proceso->paginas_usadas);
+    free(proceso); // liberar struct proceso
+    return NULL;
 }
-=======
-// }
->>>>>>> 4cc24c82fb4ffc20da474d804199a3fc0b47bf93
 
-// int buscar_marco_libre() 
-// {
-//     for (int i = 0; i < cantidad_marcos_totales; i++) {
-//         if (!bitarray_test_bit(bitmap_marcos, i)) {
-//             bitarray_set_bit(bitmap_marcos, i);
-//             return i;
-//         }
-//     }
-//     return -1;
-// }
+int buscar_marco_libre() 
+{
+    for (int i = 0; i < cantidad_marcos_totales; i++) {
+        if (!bitarray_test_bit(bitmap_marcos, i)) {
+            bitarray_set_bit(bitmap_marcos, i);
+            return i;
+        }
+    }
+    return -1;
+}
 
-// // busca la entrada en cuestion
+// busca la entrada en cuestion
 
-// t_entrada_tabla* buscar_entrada(t_tabla_paginas* tabla_raiz, int nro_pagina){
+t_entrada_tabla* buscar_entrada(t_tabla_paginas* tabla_raiz, int nro_pagina){
 
-//     t_tabla_paginas* actual = tabla_raiz;
-//     int bits_por_nivel = log2(campos_config.entradas_por_tabla);
+    t_tabla_paginas* actual = tabla_raiz;
+    int bits_por_nivel = log2(campos_config.entradas_por_tabla);
 
     for(int nivel = 0; nivel < campos_config.cantidad_niveles -1; nivel++){
         int indice = (nro_pagina >> (bits_por_nivel * (campos_config.cantidad_niveles - nivel - 1))) & ((1 << bits_por_nivel) - 1);
