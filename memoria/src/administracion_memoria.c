0b5342a23abf76a920bd7041d2861abe38b7df0a
manuEsteban22 <manuestebanseret@gmail.com>
2025-07-23 23:57:32 -0300

    retardis nenirua

--- memoria/src/administracion_memoria.c
+++ memoria/src/administracion_memoria.c
@@ -29,43 +29,7 @@ void inicializar_memoria()
 
     log_info(logger,"Memoria de usuario inicializada con %d marcos de %db cada uno",cantidad_marcos, campos_config.tam_pagina);
 }
-
-
-// t_tabla_paginas* crear_tabla(int nivel_actual)
-// {
-
-//     t_tabla_paginas* tabla = malloc(sizeof(t_tabla_paginas));
-//     if (!tabla) {
-//         log_error(logger, "Error al asignar memoria para tabla nivel %d", nivel_actual);
-//         return NULL;
-//     }
-
-//     tabla->entradas = malloc(sizeof(t_entrada_tabla) * campos_config.entradas_por_tabla);
-//     if (!tabla->entradas) {
-//         free(tabla);
-//         log_error(logger, "Error al asignar entradas para tabla nivel %d", nivel_actual);
-//         return NULL;
-//     }
-//     memset(tabla->entradas, 0, sizeof(t_entrada_tabla) * campos_config.entradas_por_tabla);
-
-
-//     for(int i = 0; i < campos_config.entradas_por_tabla; i++)
-//     {
-//         tabla->entradas[i].presencia = false;
-//         tabla->entradas[i].marco = -1;
-//         tabla->entradas[i].siguiente_tabla = NULL;
-//         if (nivel_actual < campos_config.cantidad_niveles - 1) {
-//             tabla->entradas[i].siguiente_tabla = crear_tabla(nivel_actual + 1);
-
-//             if (!tabla->entradas[i].siguiente_tabla) {
-//                 Liberar toda la tabla si falla
-//                 liberar_tabla(tabla, nivel_actual);
-//                 return NULL;
-//             }
-//         }
-//     }
-//     return tabla;
-// } 
+ 
 
 t_tabla_paginas* crear_tabla(int nivel_actual) {
     t_tabla_paginas* tabla = malloc(sizeof(t_tabla_paginas));
